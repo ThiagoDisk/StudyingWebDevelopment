4bf1ad62c088ba98c20068051c646ae97c5d35bd
ThiagoDisk <thiagodiaspenha@gmail.com>
2019-10-31 18:56:18 -0200

    Inicio

---
+++ Fundamentos/comentario.js
@@ -0,0 +1,12 @@
+//Comentario de uma linha
+console.log("Linha 1");
+
+/*
+Comentario de 
+multiplas linhas
+*/
+console.log("Linha 2");
+
+/*
+Evitar nomes genericos e siglas para constantes e variaveis.
+*/
