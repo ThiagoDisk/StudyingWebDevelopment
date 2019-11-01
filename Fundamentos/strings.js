08662eb540b8fa234e83f1f8a60cfbece20d4f00
ThiagoDisk <thiagodiaspenha@gmail.com>
2019-11-01 16:35:46 -0200

    Commit

---
+++ Fundamentos/strings.js
@@ -0,0 +1,6 @@
+const escola = "Cod3r"
+console.log(escola.charAt(4))
+console.log(escola.charAt(5))
+console.log(escola.codePointAt(3)) //Tabela Unicode para o numero 3
+console.log(escola.indexOf('3'))
+console.log(escola.substring(1))
