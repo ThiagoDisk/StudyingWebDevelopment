7e5999dbe5aa3dfa48832954868ad4a546818cd8
ThiagoDisk <thiagodiaspenha@gmail.com>
2019-11-12 14:58:14 -0200

    12-11

---
+++ Fundamentos/objeto.js
@@ -0,0 +1,14 @@
+const prod1 = {}
+prod1.nome = 'Celular Ultra Mega'
+prod1.preco = 4998.90
+prod1['Desonto Legal'] = 0.4 //Não recomendavel, evitar atributos com espaço.
+
+console.log(prod1)
+
+const  prod2  ={
+    nome:"Camisa polo",
+    preco: 79.9,
+
+}
+
+console.log(prod2)
